3b6862c750118eaa7d4537b46d033510ff115d39
Admin <admin@Admins-MacBook-Pro.local>
2023-07-10 17:12:46 +0100

    terraform configuration for my infra

---
+++ main.tf
@@ -0,0 +1,59 @@
+resource "aws_vpc" "bisoye" {
+  cidr_block       = "192.168.0.0/24"
+  instance_tenancy = "default"
+
+  tags = {
+    Name = "bisoye-infrastructure"
+  }
+}
+
+resource "aws_internet_gateway" "gw" {
+  vpc_id = aws_vpc.bisoye.id
+
+  tags = {
+    Name = "main"
+  }
+}
+
+resource "aws_subnet" "private" {
+  vpc_id     = aws_vpc.bisoye.id
+  cidr_block = "192.168.0.0/28"
+
+  tags = {
+    Name = "private"
+  }
+}
+
+resource "aws_subnet" "public" {
+  vpc_id                 = aws_vpc.bisoye.id
+  cidr_block             = "192.168.0.16/28"
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "public"
+  }
+}
+
+resource "aws_route_table" "rt" {
+    vpc_id = aws_vpc.bisoye.id
+
+    route {
+        cidr_block = "0.0.0.0/0"
+        gateway_id = aws_internet_gateway.gw.id
+
+    }
+    tags = {
+        name = "public-route-table"
+    }
+}
+
+resource "aws_route_table_association" "subnet_association" {
+    subnet_id = aws_subnet.public.id
+    route_table_id = aws_route_table.rt.id
+}
+
+
+
+
+
+
